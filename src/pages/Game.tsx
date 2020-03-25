f3a0d18ec315c1534bc82e934a6108e5ea8a498f
King Elisha <ek.chibueze@gmail.com>
2020-03-25 11:05:14 +0100

    apply rules

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -87,8 +87,28 @@ export default class Game extends Component {
 
   iteration = () => {
     console.log("running iteration...");
+
     let newBoard = this.makeEmptyBoard();
+
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.cols; x++) {
+        let neighbors = this.calculateNeighbors(this.board, x, y);
+        if (this.board[y][x]) {
+          if (neighbors === 2 || neighbors === 3) {
+            newBoard[y][x] = true;
+          } else {
+            newBoard[y][x] = false;
+          }
+        } else {
+          if (!this.board[y][x] && neighbors === 3) {
+            newBoard[y][x] = true;
+          }
+        }
+      }
+    }
+
     this.board = newBoard;
+
     this.setState({ cells: this.makeCells() });
   };
 
